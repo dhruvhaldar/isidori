c2069cd9c2ed10fdf5969568e763e92479e24adc
Dhruv Haldar <dhruv.haldar@tridiagonal.com>
2026-03-13 11:33:52 +0530

    add scientific notation

--- src/components/system-chart.tsx
+++ src/components/system-chart.tsx
@@ -6,6 +6,10 @@ interface SystemChartProps {
   data: { time: number; y: number; d: number }[];
 }
 
+const formatScientific = (value: number) => {
+  return value.toExponential(2);
+};
+
 export function SystemChart({ data }: SystemChartProps) {
   return (
     <div style={{ width: '100%', height: 400 }}>
@@ -21,8 +25,11 @@ export function SystemChart({ data }: SystemChartProps) {
         >
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="time" label={{ value: 'Time (s)', position: 'insideBottomRight', offset: -5 }} />
-          <YAxis label={{ value: 'Value', angle: -90, position: 'insideLeft' }} />
-          <Tooltip />
+          <YAxis 
+            tickFormatter={formatScientific}
+            label={{ value: 'Value', angle: -90, position: 'insideLeft' }} 
+          />
+          <Tooltip formatter={(value: number) => formatScientific(value)} />
           <Legend />
           <Line type="monotone" dataKey="y" stroke="#8884d8" name="Output y(t)" dot={false} strokeWidth={2} />
           <Line type="monotone" dataKey="d" stroke="#82ca9d" name="Disturbance d(t)" dot={false} strokeDasharray="5 5" />
